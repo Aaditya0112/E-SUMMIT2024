a9357b49966b28f049bc03e034f91a05e1d35deb
Aaditya0112 <rajaaditya0112@gmail.com>
2024-12-03 18:28:32 +0530

    update in circular Carousel

--- JS/index.js
+++ JS/index.js
@@ -28,22 +28,47 @@ workshops
  */
 
 const carousel = document.querySelector('.carousel-container');
-const quantity = document.querySelectorAll('.items').length - 1;
+const quantity = document.querySelectorAll('.items').length;
+const items = document.querySelectorAll('.items')
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
+
+const indexing = (i) => {
+    let idx= i%quantity;
+    if(idx<0){
+        idx = idx*-1  
+    }else if (idx>0){
+        idx = 11-idx  
+    }else{
+        idx = 0;
+    }
+    return idx;
+}
+
+const currentSlide = (items, idx) => {
+    items.forEach((link) => {
+        link.classList.remove('active');
+    })
+    items[idx].classList.add('active')
+}
 
 let i =0;
 
 next.addEventListener('click', () => {
-    carousel.style.rotate = `${-(++i)*(360/quantity)}deg`
-    // document.querySelectorAll('.items')[i%quantity].classList.add('active');
-    // console.log(i%quantity);
-    
+    carousel.style.rotate = `${-(++i)*(360/quantity)}deg`;
+
+    let index = indexing(i)
+    currentSlide(items, index)
+
+
 });
 
 prev.addEventListener('click', () => {
     carousel.style.rotate = `${-(--i)*(360/quantity)}deg`
-    // console.log(i%quantity);
+
+    let index = indexing(i)
+    currentSlide(items, index)
+
      
 })
 
